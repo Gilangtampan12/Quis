6c488d4d6ce2b86d595e6dc75a8a416c44620854
Gilangtampan12 <gilangtampan1208@gmail.com>
2024-12-07 16:06:11 +0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Apa kepanjangan dari HTML?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Apa kepanjangan dari CSS?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Apa kepanjangan dari PHP?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Apa kepanjangan dari SQL?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Apa kepanjangan dari XML?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Berikut adalah bidang pekerjaan dengan spesialisasi dalam hal web developer, kecuali?",
+    answer: "Web developer dan web administrator",
+    options: [
+      "Webmaster dan senior web developer",
+      "Front end developer dan web developer",
+      "Web administrator dan senior web administrator",
+      "Web developer dan web administrator"
+      ]
+  },
+  {
+    num: 7,
+    question: "Seorang system analyst mempunyai tugas utama yaitu?",
+    answer: "Merancang sistem",
+    options: [
+      "Merubah sistem",
+      "Mengembangkan aplikasi dan database",
+      "Merancang sistem",
+      "Membuat program",
+      ]
+  },
+  {
+    num: 8,
+    question: "Salah satu kelebihan jaringan kabel dibandingkan jaringan nirkabel yaitu?",
+    answer: "Lebih aman dari gangguan sinyal dari luar",
+    options: [
+      "Lebih aman dari gangguan sinyal dari luar",
+      "Lebih fleksibel",
+      "Lebih mudah dalam proses instalasi",
+      "Lebih mudah dalam pemeliharaan",
+    ]
+  },
+  {
+    num : 9,
+    question: "Segala sesuatu yang menyangkut keamanan dikenal dengan istilah?",
+    answer: "Security",
+    options: [
+      "Komputer",
+      "Security",
+      "Hardware",
+      "Brainware",
+      ]
+  },
+  {
+    num : 10,
+    question: "Google-IT Support adalah salah satu sertifikasi IT bagi seseorang yang ingin mengembangkan dirinya dengan profesi sebagai?",
+    answer: "Helpdesk",
+    options: [
+      "Developer",
+      "Programmer",
+      "Helpdesk",
+      "Mobile developer",
+      ]
+  }
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
